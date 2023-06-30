6bcfd92a444db2a037c3333b0a660b32b841db8b
caipira113 <caipira@libnare.net>
2023-07-01 04:20:54 +0900

    First commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM alpine:3.16
+
+RUN apk --no-cache add openssh-client
+
+ADD run.sh /run.sh
+
+# Security fix for CVE-2016-0777 and CVE-2016-0778
+RUN echo -e 'Host *\nUseRoaming no' >> /etc/ssh/ssh_config
+
+ENTRYPOINT ["/bin/sh", "-c", "/run.sh"]
---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ -z ${REMOTE_SERVER_IP+x} ]; then
+    REMOTE_SERVER_IP="127.0.0.1"
+fi
+
+if  [ -z ${SSH_PORT+x} ] ; then
+    SSH_PORT="22"
+fi
+
+if [ -z ${LOCAL_PORT+x} ] || [ -z ${REMOTE_PORT+x} ] || [ -z ${SSH_BASTION_HOST+x} ] || [ -z ${SSH_USER+x} ] ; then 
+    echo "Some vars are not set"; 
+    exit 1
+fi
+
+echo "starting SSH proxy $LOCAL_PORT:$REMOTE_SERVER_IP:$REMOTE_PORT on $SSH_USER@$SSH_BASTION_HOST:$SSH_PORT"
+
+/usr/bin/ssh \
+-NTC -o ServerAliveInterval=60 \
+-o GatewayPorts=true \
+-o ExitOnForwardFailure=yes \
+-o StrictHostKeyChecking=no \
+-L $LOCAL_PORT:$REMOTE_SERVER_IP:$REMOTE_PORT \
+$SSH_USER@$SSH_BASTION_HOST \
+-p $SSH_PORT \
+-i $SSH_KEY_PATH
